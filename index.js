0c09f688edd293f42aa5efe7c0fe014b0b7661b0
juan Hernandez <juan.hernandeznoriega01@utrgv.edu>
2017-06-14 14:52:35 -0500

    finished project

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+
+var messages = [];
+
+app.get('/messages', function (req, res, next)  {
+    res.status(200).json({messages: messages})
+});
+
+app.post('/messages', function (req, res, next) {
+    console.log(req.body);
+    messages.push({message: req.body.message, time: new Date()});
+    res.status(200).json({messages: messages});
+});
+
+
+app.listen(3000, function() {
+    console.log('server running')
+});
+
